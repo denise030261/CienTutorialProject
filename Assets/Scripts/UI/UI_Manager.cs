23b5b5070dfae7937d3d2b7031fc9116b5f13a4f
Younghyeon Park <adecay@192.168.nate.com>
2023-07-20 16:42:51 +0900

    feat: user score

--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -9,6 +9,8 @@ public class UI_Manager : MonoBehaviour
     public Image HP_Image;
     public Image MP_Image;
     public Text ScoreText;
+    public Text highScoreText;
+    public Text currentScoreText;
 
     public GameObject GameOverImage;
     public static UI_Manager Instance { get; private set; } = null;
@@ -17,6 +19,7 @@ public class UI_Manager : MonoBehaviour
     {
         Instance = this;
     }
+
     private void Update()
     {
         ScoreText.text = "Score : " + Player.Instance.score.ToString();
@@ -26,18 +29,36 @@ public class UI_Manager : MonoBehaviour
             // ???? ???? ?????? ????
         }
     }
+
     public void SetUI_HP(int current, int max)
     {
         HP_Image.fillAmount = (float)current / max;
     } // HP ????
+
     public void SetUI_MP(int current, int max)
     {
         MP_Image.fillAmount = (float)current / max;
     } // MP ????
+
+    public void SetScore(int currentScore)
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+
+        if (currentScore > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", currentScore);
+            highScore = currentScore;
+        }
+
+        highScoreText.text = "HIGH SCORE: " + highScore.ToString();
+        currentScoreText.text = "CURRENT SCORE: " + currentScore.ToString();
+    }
+
     public void OnClick_Retry()
     {
         
     } // ???? ????
+
     public void OnClick_LoadToMain()
     {
         
--- Assets/main/MainMenuController.cs
+++ Assets/main/MainMenuController.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public Text highScoreText;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        highScoreText.text = "HIGH SCORE: " + highScore.ToString();
+    }
+
     public void NewGame()
     {
         SceneManager.LoadScene("SampleScene");
