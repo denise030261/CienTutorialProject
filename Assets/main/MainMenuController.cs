3d5368c1aaef6d63b0e7e7e9d3047b46dc6d5c05
unikim-03 <unikim031101@naver.com>
2023-07-20 18:27:35 +0900

    옵션1

--- Assets/main/MainMenuController.cs
+++ Assets/main/MainMenuController.cs
@@ -2,18 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    public Text highScoreText;
-
-    private void Start()
-    {
-        int highScore = PlayerPrefs.GetInt("HighScore", 0);
-        highScoreText.text = "HIGH SCORE: " + highScore.ToString();
-    }
-
     public void NewGame()
     {
         SceneManager.LoadScene("SampleScene");
@@ -26,14 +17,23 @@ public class MainMenuController : MonoBehaviour
 
     public void OpenSettings()
     {
-        SceneManager.LoadScene("option");
+        GameObject settingsPrefab = Resources.Load<GameObject>("optionscene");
+
+        if (settingsPrefab != null)
+        {
+            Instantiate(settingsPrefab);
+        }
+        else
+        {
+            Debug.LogError("Could not load the 'optionscene' prefab.");
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-    
+
     public void BackSettings()
     {
         SceneManager.LoadScene("mainmenu");
