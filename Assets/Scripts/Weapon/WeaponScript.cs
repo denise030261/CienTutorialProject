8fa6e1b3c330650e60ec543f58847525edf1ef80
Younghyeon Park <adecay@Younghyeonui-MacBookPro.local>
2023-07-22 09:47:27 +0900

    fix: attack SFX

--- Assets/Scripts/Weapon/WeaponScript.cs
+++ Assets/Scripts/Weapon/WeaponScript.cs
@@ -11,24 +11,19 @@ public class WeaponScript : MonoBehaviour
     public int instantiatedWeapon;
     public GameObject weaponInstance;
 
-    //private AudioSource audioSource;
-    //public AudioClip attackClip;
-    public AudioSource[] audioSources; // ?????? ????
+    private AudioSource audioSource;
+    public AudioClip[] attackClips;
 
     private void Awake()
     {
         Instance = this;
-        //audioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
     // Start is called before the first frame update
     void Start()
     {
         currentWeapon = 0;
         weaponInstance = null;
-        foreach (var audioSource in audioSources)
-        {
-            audioSource.enabled = false;
-        } // ?????? ????????
     }
 
     // Update is called once per frame
@@ -47,30 +42,25 @@ public class WeaponScript : MonoBehaviour
 
             if (currentWeapon == 1 || currentWeapon == 2)
             {
+                audioSource.loop = false;
                 Destroy(weaponInstance, .1f);
-                audioSources[1].enabled = false;
-                audioSources[2].enabled = false;
+            }
+            else
+            {
+                audioSource.loop = true;
             }
 
-            for(int i=0;i<4;i++)
-            {
-                if(currentWeapon==i)
-                {
-                    audioSources[i].enabled = true;
-                    audioSources[i].Play();
-                }
-            }
-            //audioSource.clip = attackClip;
-            //audioSource.Play();
+
+            audioSource.clip = attackClips[currentWeapon];
+            audioSource.Play();
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             if (instantiatedWeapon == 0 || instantiatedWeapon == 3)
             {
+                audioSource.Stop();
                 Destroy(weaponInstance);
-                audioSources[0].enabled = false;
-                audioSources[3].enabled = false;
             }
         }
 
